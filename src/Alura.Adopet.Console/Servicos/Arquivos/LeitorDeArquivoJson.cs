d6777bb5e16ff46222cb6919f3cb5aebe8111324
Gustavo Carvalho <gustavo.carvalhoas@hotmail.com>
2024-08-08 21:07:05 -0300

    @GustavoCarvalho25 - Aplicando princípio D (Inversão de dependência) e refatorando código

---
+++ src/Alura.Adopet.Console/Servicos/Arquivos/LeitorDeArquivoJson.cs
@@ -0,0 +1,27 @@
+﻿using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Servicos.Abstracoes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Alura.Adopet.Console.Servicos.Arquivos
+{
+    public class LeitorDeArquivoJson : ILeitorDeArquivos
+    {
+        private string caminhoDoArquivo;
+
+        public LeitorDeArquivoJson(string caminhoDoArquivo)
+        {
+            this.caminhoDoArquivo = caminhoDoArquivo;
+        }
+
+        public IEnumerable<Pet> RealizaLeitura()
+        {
+            using var stream = new FileStream(caminhoDoArquivo, FileMode.Open, FileAccess.Read);
+            return JsonSerializer.Deserialize<IEnumerable<Pet>>(stream) ?? Enumerable.Empty<Pet>();
+        }
+    }
+}
